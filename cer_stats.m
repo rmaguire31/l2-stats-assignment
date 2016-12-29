55cde3173e0ee00521d45afc86ab953cdefd517f
rmaguire31 <rmaguire31@gmail.com>
2016-12-29 11:43:28 +0000

    Initial commit

---
+++ cer_stats.m
@@ -0,0 +1,13 @@
+function cer_stats()
+%CER_STATS Electricity consumption statistics assignmnet
+%   Code submission by: [replace with your name]
+
+% Load the data from this directory
+
+load CER_smartmeters.mat
+
+% Do the computing for the assignment....
+
+
+end
+
